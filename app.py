0b976939d009f5bed554c9f0158f5ac721acdc4e
Vasya Zhukov <vzukov623@gmail.com>
2024-03-25 09:39:47 +0300

    date update in data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 import random
 
 app = Flask(__name__)
@@ -25,6 +26,7 @@ class Events(db.Model):
     first_opponent = db.Column(db.String(100))
     second_opponent = db.Column(db.String(100))
     tournoment = db.Column(db.String(100))
+    date = db.Column(db.DateTime)
     first_score = db.Column(db.Integer)
     second_score = db.Column(db.Integer)
 
@@ -34,7 +36,8 @@ class Events(db.Model):
 class Bets(db.Model):
     bet_id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.Integer, nullable=False)
-    username = db.Column(db.String(25), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime)
     first_score = db.Column(db.Integer, nullable=False)
     second_score = db.Column(db.Integer, nullable=False)
     coins = db.Column(db.Integer, nullable=False)
@@ -45,6 +48,9 @@ class Bets(db.Model):
 
 @app.route('/')
 def main():
+    event = Events(first_opponent = "Ipswich", second_opponent = "Pochatok", tournoment = "Freak Championship", date = datetime.datetime.now(), first_score = None, second_score = None, )
+    db.session.add(event)
+    db.session.commit()
     return render_template("main.html")
 
 @app.route('/login', methods=["POST", "GET"])
@@ -53,7 +59,7 @@ def login():
         form = request.form
         username = form["username"]
         password = form["password"]
-        check = Users.query.filter_by(username=username, password=password).first()
+        check = Users.query.filter_by(username = username, password = password).first()
         if (check is None):
             print("no")
         else:
@@ -69,7 +75,7 @@ def sign_up():
         if (len(username) > 25 or len(password) > 25):
             print("mistake, username and password must include less than 25 letters")
         else:
-            check = Users.query.filter_by(username=username).first()
+            check = Users.query.filter_by(username = username).first()
             if (check is None):
                 user = Users(username = username, password = password, coins = 100)
                 db.session.add(user)
